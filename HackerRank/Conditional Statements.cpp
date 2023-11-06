1c78fb8f8af29bdc9b24c5bd9a3bf350ab4d04da
Japneet Kaur <121337325+Japneet001@users.noreply.github.com>
2023-11-06 17:21:58 +0530

    code

---
+++ HackerRank/Conditional Statements.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+
+
+
+int main()
+{
+    string n_temp;
+    getline(cin, n_temp);
+
+    int n = stoi(ltrim(rtrim(n_temp)));
+    const char* num[9]={"one","two","three","four","five","six","seven","eight","nine"};
+
+    // Write your code here
+    if (1<=n && n<=9){
+        cout << num[n-1] << endl;
+    }
+    else{
+        cout << "Greater than 9" << endl;
+    }
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
