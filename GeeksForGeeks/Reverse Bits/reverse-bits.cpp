d1ef48d9a2ca8c0fe4e3c64a9032b097c2ebeeae
Japneet Kaur <121337325+Japneet001@users.noreply.github.com>
2024-04-17 07:30:24 +0530

    Added Solution - GfG to GitHub

---
+++ GeeksForGeeks/Reverse Bits/reverse-bits.cpp
@@ -0,0 +1,34 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    long long reversedBits(long long x) {
+        // code here
+        long long ans = 0;
+        for (int i = 0 ; i < 32 ; i++){
+            ans = ans << 1;
+            ans = ans | (x & 1);
+            x = x >> 1;
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        long long X;
+        
+        cin>>X;
+
+        Solution ob;
+        cout << ob.reversedBits(X) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
